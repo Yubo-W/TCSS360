a153dea92dd18c40163def64c044cd657a4f60ae
StasKat <stask2@uw.edu>
2019-11-10 14:07:41 -0800

    changed name, and working on popup for user and pass

--- 360Workspace/360Proj/src/Model/UserProfile.java
+++ 360Workspace/360Proj/src/Model/UserProfile.java
@@ -1,6 +1,6 @@
 package Model;
 
-public class UserSettings {
+public class UserProfile {
 	
 	private String myUserName;
 	
--- 360Workspace/360Proj/src/View/UserEmailAndNameWindow.java
+++ 360Workspace/360Proj/src/View/UserEmailAndNameWindow.java
@@ -1,9 +1,26 @@
 package View;
 
+import javax.swing.JFrame;
+
 public class UserEmailAndNameWindow {
+
+	
 	
 	//Create a window to ask the user for username and email pass to current userbrdige. 
 
+	public void generateEmailandUserWindow() {
+		
+		final JFrame userandEmailFrame = new JFrame(); 
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 	
 }
