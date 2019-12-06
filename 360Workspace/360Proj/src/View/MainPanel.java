ca94a1844f3dced44d36966f86fd007a43954a19
Trevor Nichols <trevorn3@uw.edu>
2019-12-05 16:07:02 -0800

    Working on updating tag list (left panel)

--- 360Workspace/360Proj/src/View/MainPanel.java
+++ 360Workspace/360Proj/src/View/MainPanel.java
@@ -3,16 +3,27 @@ package View;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.filechooser.FileSystemView;
+
+import Controller.CurrentUserBridgeDisplay;
+import Model.FileMap;
+import Model.Item;
+import Model.Tag;
 
 
 public class MainPanel extends JPanel {
@@ -27,16 +38,22 @@ public class MainPanel extends JPanel {
 	private JButton itemAdd;
 	private JButton itemDelete;
 	private JList<String> tagList;
+	private FileMap storage;
+	private JPanel tagScroll;
+	private DefaultListModel<String> tagNames;
 
 
 	public MainPanel() {
+		storage = new FileMap();
+		tagNames = new DefaultListModel<String>();
 		setLayout(new BorderLayout());
 		createTagWindow();
 		createItemWindow();
 	}
 	
 	private void createTagWindow() {
-		JPanel projectScroll = new JPanel(new BorderLayout());
+		
+		tagScroll = new JPanel(new BorderLayout());
 		JPanel projectButtn = new JPanel(new FlowLayout());
 		
 		/*
@@ -46,15 +63,16 @@ public class MainPanel extends JPanel {
 		 * confirm delete. the listener should also refresh the projectScroll
 		 * panel, so that the updates are visible
 		 */
-		tagAdd = new JButton("Add Project");
+		tagAdd = new JButton("Add Tag");
+		addTagListener();
 		tagDelete= new JButton("Delete Project");
+		tagDelete.setEnabled(false);
 		
 		projectButtn.add(tagAdd);
 		projectButtn.add(tagDelete);
 		
-		projectScroll.add(projectButtn, BorderLayout.NORTH);
+		tagScroll.add(projectButtn, BorderLayout.NORTH);
 	
-		String test[] = {"Trent", "did", "not", "contribute", "to", "this", "project", "H", "I", "J"};
 		
 		//add all the key values from our map to this JList.
 		
@@ -63,16 +81,16 @@ public class MainPanel extends JPanel {
 		 * another window with all the values for the selected key/keys
 		 * should refresh the window containing all values.
 		*/
-		tagList = new JList<String>(test);
+		tagList = new JList<String>(storage.getTagNames());
 		
 		JScrollPane projects = new JScrollPane(tagList);
-		projectScroll.add(projects, BorderLayout.CENTER);
+		tagScroll.add(projects, BorderLayout.CENTER);
 		
-		add(projectScroll, BorderLayout.WEST);
+		add(tagScroll, BorderLayout.WEST);
 	}
 	
 	private void createItemWindow() {
-		//Documents window is below this comment
+
 		//attempting to get the documents window to display correctly.
 		JPanel documents = new JPanel(new BorderLayout());
 		JPanel icons = new JPanel(new GridLayout(0, 10, 6, 6));
@@ -82,6 +100,8 @@ public class MainPanel extends JPanel {
 		JPanel documentButtn = new JPanel(new FlowLayout());
 		itemAdd = new JButton("Add File");
 		itemDelete = new JButton("Delete File");
+		itemAdd.setEnabled(false);
+		itemDelete.setEnabled(false);
 		documentButtn.add(itemAdd);
 		documentButtn.add(itemDelete);		
 		documents.add(documentButtn, BorderLayout.NORTH);
@@ -92,4 +112,32 @@ public class MainPanel extends JPanel {
 		
 		add(documents, BorderLayout.CENTER);
 	}
+	
+	private void addTagListListener() {
+		
+	}
+	
+	private void addTagListener() {
+		tagAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent theEvent) {
+				JTextField tag = new JTextField(20);
+
+	    		
+	    		//creates a panel to grab user and email
+	    		JPanel tagPanel = new JPanel();
+	    		tagPanel.add(new JLabel("Tag: "));
+	    		tagPanel.add(tag);
+	    			
+	    		//Creating the ok and cancel button. 
+	    		int result = JOptionPane.showConfirmDialog(null, tagPanel, "Add Tag", JOptionPane.OK_CANCEL_OPTION);
+	    		
+	    		if (result == JOptionPane.OK_OPTION) {
+	    			storage.addCategory(new Tag(tag.getText()));
+	    			tagNames.addElement(tag.getText());
+	    			repaint();
+	    		}
+			}
+		});
+	}
 }
+	
