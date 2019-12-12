c9c6ad3552971e67701a9672d9c61ed6dfd4719c
Trent Julich <tjulich1@gmail.com>
2019-12-11 18:13:08 -0800

    added documentation to several classes

--- 360Workspace/360Proj/src/Model/UserProfile.java
+++ 360Workspace/360Proj/src/Model/UserProfile.java
@@ -1,36 +1,60 @@
 package Model;
 
+/**
+ * Class representing a user profile, storing the email and user name.
+ * 
+ * @author Trent Julich
+ * @date 12/01/19
+ */
 public class UserProfile {
 	
 	private String myUserName;
 	
 	private String myEmailAddress;
 	
-	//TODO categories of this user. what ever data type we choose.
-	
-	
+	/**
+	 * Method to set the profiles user name.
+	 * 
+	 * @param theUserName The new user name to set.
+	 */
 	public void setUserName(final String theUserName) {
 		this.myUserName = theUserName;
 	}
 	
-	public void setEmailAddress(final String theUserName) {
-		this.myEmailAddress = theUserName;
+	/**
+	 * Method to set the profiles email address.
+	 * 
+	 * @param newEmailAddress The new email address. 
+	 */
+	public void setEmailAddress(final String newEmailAddress) {
+		this.myEmailAddress = newEmailAddress;
 	}
 	
+	/**
+	 * Method used to get the profiles user name.
+	 * 
+	 * @return String - the user name attached to the profile.
+	 */
 	public String getUserName() {
 		return this.myUserName;
 	}
 	
+	/**
+	 * Method used to get the profiles email address.
+	 * 
+	 * @return String - The profiles email address.
+	 */
 	public String getUserEmail() {
 		return this.myEmailAddress;
 	}
 	
-	
+	/**
+	 * Method used to convert the profile to a String representation.
+	 * 
+	 * @returns String - A string representation of the user profile.
+	 */
 	public String toString() {
 		return "User: " + myUserName + "    " + " Email: " + myEmailAddress; 
 	}
 	
-	//TODO create a get and set for the categories. 
-	
-
 }
